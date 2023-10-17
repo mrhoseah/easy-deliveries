ebbf9b4f7ee3a572ba67685134f21f8b934a6ded
Hoseah Kiplang'at <hoseahkplgt@gmail.com>
2023-10-17 06:33:27 -0700

    first commit

---
+++ items.js
@@ -0,0 +1,435 @@
+export const foodItem= [
+    {
+        id: 1,
+        name: 'Margherita Pizza(L)',
+        category : 'pizza',
+        rating : 4,
+        price: 1100,
+        img: 'tools/foods/pizza 2.jpg',
+        quantity: 1
+    },
+    {
+        id: 2,
+        name: 'Cheese Pizza',
+        category : 'pizza',
+        rating : 4,
+        price: 800,
+        img: 'tools/foods/pizza slice 1.jpg',
+        quantity: 1
+
+    },
+    {
+        id: 3,
+        name: 'Pepperoni Pizza',
+        category: 'pizza',
+        rating : 4,
+        price: 550,
+        img: 'tools/foods/pizza slice.jpg',
+        quantity: 1
+
+    },
+    {
+        id: 4,
+        name: 'BBQ Chicken Pizza',
+        category: 'pizza',
+        rating : 4,
+        price: 550,
+        img: 'tools/foods/pizza 3.jpg',
+        quantity: 1
+
+    },
+    {
+        id: 5,
+        name: 'Mutton Special Pizza',
+        category: 'pizza',
+        rating : 4,
+        price: 800,
+        img: 'tools/foods/chips.jpeg',
+        quantity: 1
+
+    },
+    {
+        id: 6,
+        name: 'BBQ Beef Pizza',
+        category: 'pizza',
+        rating : 4,
+        price: 900,
+        img: 'tools/foods/pizza 1.jpg',
+        quantity: 1
+
+    },
+ 
+    {
+        id: 8,
+        name: 'Plain Chips',
+        category: 'fries',
+        rating : 4,
+        price: 120,
+        img: 'tools/Snacks/plain chips.jpeg',
+        quantity: 1
+
+    },
+    {
+        id: 9,
+        name: 'Chips Masala',
+        category: 'fries',
+        rating : 4,
+        price: 230,
+        img: 'tools/Snacks/masala.jpeg',
+        quantity: 1
+
+    },
+    {
+        id: 10,
+        name: 'Chips Burger',
+        category: 'fries',
+        rating : 4,
+        price: 350,
+        img: 'tools/Snacks/chips Burger.jpeg',
+        quantity: 1
+
+    },
+    {
+        id: 11,
+        name: 'Plain Burger',
+        category: 'fries',
+        rating : 4,
+        price: 250,
+        img: 'tools/Snacks/download.jpeg',
+        quantity: 1
+
+    },
+    {
+        id: 12,
+        name: 'Home Fries',
+        category: 'fries',
+        rating : 4,
+        price: 150,
+        img: 'tools/Snacks/home fries.jpg',
+        quantity: 1
+
+    },
+    {
+        id: 13,
+        name: 'Sausages',
+        category: 'fries',
+        rating : 4,
+        price: 100,
+        img: 'tools/Snacks/sausage.jpg',
+        quantity: 1
+
+    },
+
+    {
+        id: 14,
+        name: 'Smokies',
+        category: 'fries',
+        rating : 4,
+        price: 50,
+        img: 'tools/Snacks/smokies.jpg',
+        quantity: 1
+
+    },
+    {
+        id: 14,
+        name: 'Chips Grilled Kuku',
+        category: 'fries',
+        rating : 4,
+        price: 50,
+        img: 'tools/Snacks/Grilled-chicken-n-chips.jpg',
+        quantity: 1
+
+    },
+    {
+        id: 15,
+        name: 'Black Forest 1kg',
+        category: 'cakes',
+        rating : 4,
+        price:900,
+        img: 'tools/foods/cake 1.jpg',
+        quantity: 1
+
+    },
+    {
+        id: 16,
+        name: 'Cup Cakes',
+        category: 'cakes',
+        rating : 4,
+        price: 150,
+        img: 'tools/Babbies/bb soaps.jpg',
+        quantity: 1
+
+    },
+    {
+        id: 17,
+        name: 'White Forest 1kg',
+        category: 'cakes',
+        rating : 4,
+        price: 2000,
+        img: 'tools/Foods/cake flower.jpg',
+        quantity: 1
+
+    },
+    {
+        id: 18,
+        name: 'Passion  1.5kg',
+        category: 'cakes',
+        rating : 4,
+        price: 2500,
+        img: 'tools/foods/black forest.jpg',
+        quantity: 1
+
+    },
+    {
+        id: 19,
+        name: 'Caramel Cake 1kg',
+        category: 'cakes',
+        rating : 4,
+        price: 1800,
+        img: 'tools/foods/cake 2.jpg',
+        quantity: 1
+
+    },
+    {
+        id: 20,
+        name: 'Vanilla Cake 1kg',
+        category: 'cakes',
+        rating : 4,
+        price: 2000,
+        img: 'tools/Foods/cake 3.jpg',
+        quantity: 1
+
+    },
+    {
+        id: 21,
+        name: 'Freash from Farm',
+        category: 'breakfast',
+        rating : 4,
+        price: 150,
+        img: 'tools/Groceries/1687330285656.jpg',
+        quantity: 1
+
+    },
+    {
+        id: 22,
+        name: 'Freash from Farm',
+        category: 'breakfast',
+        rating : 4,
+        price: 250,
+        img: 'tools/Groceries/1687330292920.jpg',
+        quantity: 1
+
+    },
+    {
+        id: 23,
+        name: 'Freash from Farm',
+        category: 'breakfast',
+        rating : 4,
+        price: 150,
+        img: 'tools/Groceries/1687330315730.jpg',
+        quantity: 1
+
+    },
+    {
+        id: 24,
+        name: 'Freash from Farm',
+        category: 'breakfast',
+        rating : 4,
+        price: 100,
+        img: 'tools/Groceries/vegetables 4.jpg',
+        quantity: 1
+
+    },
+    {
+        id: 24,
+        name: 'Freash from Farm',
+        category: 'breakfast',
+        rating : 4,
+        price: 100,
+        img: 'tools/Groceries/vegetables 4.jpg',
+        quantity: 1
+
+    },
+    {
+        id: 25,
+        name: 'Freash from Farm',
+        category: 'breakfast',
+        rating : 4,
+        price: 350,
+        img: 'tools/Groceries/1687330329506.jpg',
+        quantity: 1
+
+    },
+    {
+        id: 26,
+        name: 'Freash from Farm',
+        category: 'breakfast',
+        rating : 4,
+        price: 50,
+        img: 'tools/Groceries/1687330322523.jpg',
+        quantity: 1
+
+    },
+    {
+        id: 27,
+        name: 'Freash from Farm',
+        category: 'breakfast',
+        rating : 4,
+        price: 250,
+        img: 'tools/drinks/almond milk.jpg',
+        quantity: 1
+
+    },
+    {
+        id: 28,
+        name: 'Ariel',
+        category: 'dishes',
+        rating : 4,
+        price: 150,
+        img: 'tools/Detergents/ariel.jpg',
+        quantity: 1
+
+    },
+    {
+        id: 29,
+        name: 'Liquid Omo',
+        category: 'dishes',
+        rating : 4,
+        price: 455,
+        img: 'tools/Detergents/omo 2.5.jpg',
+        quantity: 1
+
+    },
+    {
+        id: 30,
+        name: 'Chicken curry',
+        category: 'dishes',
+        rating : 4,
+        price: 150,
+        img: 'tools/drinks/fanta1.avif',
+        quantity: 1
+
+    },
+    {
+        id: 31,
+        name: 'Chicken curry',
+        category: 'dishes',
+        rating : 4,
+        price: 150,
+        img: 'tools/drinks/fanta1.avif',
+        quantity: 1
+
+    },
+    {
+        id: 32,
+        name: 'Chicken curry',
+        category: 'dishes',
+        rating : 4,
+        price: 150,
+        img: 'tools/drinks/fanta1.avif',
+        quantity: 1
+
+    },
+    {
+        id: 33,
+        name: 'Chicken curry',
+        category: 'dishes',
+        rating : 4,
+        price: 150,
+        img: 'tools/drinks/fanta1.avif',
+        quantity: 1
+
+    },
+    {
+        id: 34,
+        name: 'Cold Coke',
+        category: 'drinks',
+        rating : 4,
+        price: 70,
+        img: 'tools/drinks/cocacola 2.avif',
+        quantity: 1
+
+    },
+    {
+        id: 35,
+        name: 'Cold Fanta',
+        category: 'drinks',
+        rating : 4,
+        price: 70,
+        img: 'tools/drinks/fanta1.avif',
+        quantity: 1
+
+    },
+    {
+        id: 36,
+        name: 'Cold Fanta',
+        category: 'drinks',
+        rating : 4,
+        price: 70,
+        img: 'tools/drinks/fanta1.avif',
+        quantity: 1
+
+    },
+    {
+        id: 36,
+        name: 'Cold Fanta',
+        category: 'drinks',
+        rating : 4,
+        price: 70,
+        img: 'tools/drinks/fanta1.avif',
+        quantity: 1
+
+    },
+ 
+    {
+        id: 38,
+        name: 'Red Bull',
+        category: 'drinks',
+        rating : 4,
+        price: 250,
+        img: 'tools/drinks/red bull.avif',
+        quantity: 1
+
+    },
+    {
+        id: 39,
+        name: '500ml Delmonte',
+        category: 'drinks',
+        rating : 4,
+        price: 200,
+        img: 'tools/drinks/delmonte1.avif',
+        quantity: 1
+
+    },
+    {
+        id: 40,
+        name: '500ml Purifed Water',
+        category: 'drinks',
+        rating : 4,
+        price: 60,
+        img: 'tools/drinks/water 1.jpg',
+        quantity: 1
+
+    },
+    {
+        id: 41,
+        name: '300ml Cold Fanta',
+        category: 'drinks',
+        rating : 4,
+        price: 70,
+        img: 'tools/drinks/fanta1.avif',
+        quantity: 1
+
+    },
+    {
+        id: 42,
+        name: '300ml Cold Coke',
+        category: 'drinks',
+        rating : 4,
+        price: 70,
+        img: 'tools/drinks/cocacola1.avif ',
+        quantity: 1
+
+    },
+     
+];
